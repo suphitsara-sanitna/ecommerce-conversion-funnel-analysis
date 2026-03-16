f539266c5d460c14860a84cdbf2cbe99fa379005
Suphitsara Sanitna <sanitnafirst@gmail.com>
2026-03-16 13:09:09 +0700

    Update 07_checkout_dropoff.sql

--- scripts/07_checkout_dropoff.sql
+++ scripts/07_checkout_dropoff.sql
@@ -19,21 +19,21 @@ SQL Functions Used:
 
 --7.1 What is the overall checkout abandonment rate?
 SELECT 
-   COUNT(DISTINCT session_id) as cart_abandon,
-   (SELECT COUNT(DISTINCT CASE WHEN event_type = 'checkout' THEN session_id END) FROM events) as cart_sessions,
+   COUNT(DISTINCT session_id) as checkout_abandon,
+   (SELECT COUNT(DISTINCT CASE WHEN event_type = 'checkout' THEN session_id END) FROM events) as checkout_sessions,
    ROUND(100.0* COUNT(DISTINCT session_id)/
-   (SELECT COUNT(DISTINCT CASE WHEN event_type = 'checkout' THEN session_id END) FROM events),2) as cart_abandon_rate
+   (SELECT COUNT(DISTINCT CASE WHEN event_type = 'checkout' THEN session_id END) FROM events),2) as checkout_abandon_rate
 FROM events
 WHERE event_type = 'checkout'
 AND session_id NOT IN (
     SELECT DISTINCT session_id 
     FROM events 
     WHERE event_type = 'purchase' )
-
+    
 --7.2 Which device has the most checkout abandonment sessions?
 SELECT 
    s.device as device,
-   COUNT(DISTINCT e.session_id) as sessions
+   COUNT(DISTINCT e.session_id) as check_out_abandon_sessions
 FROM events e
 LEFT JOIN sessions s on s.session_id = e.session_id
 WHERE e.event_type = 'checkout'
@@ -47,7 +47,7 @@ ORDER BY sessions DESC
 --7.3 Which traffic source has the most checkout abandonment sessions?
 SELECT 
    s.source as source,
-   COUNT(DISTINCT e.session_id) as sessions
+   COUNT(DISTINCT e.session_id) as check_out_abandon_sessions
 FROM events e
 LEFT JOIN sessions s on s.session_id = e.session_id
 WHERE e.event_type = 'checkout'
